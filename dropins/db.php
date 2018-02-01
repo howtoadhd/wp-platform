1ddfc952734acc919e47bc7619d95fb0d0b495b3
LoreleiAurora (from Travis CI) <iam@l5i.me>
2018-02-01 16:47:41 +0000

    Deploy howtoadhd/wp-platform to github.com/howtoadhd/wp-platform.git:master

---
+++ dropins/db.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * WP Platform.
+ *
+ * @package HowToADHD\WPPlatform
+ * @link    https://github.com/howtoadhd/wp-platform
+ * @license https://opensource.org/licenses/GPL-2.0 GPLv2+
+ */
+
+namespace HowToADHD\WPPlatform;
+
+PlatformFactory::create()
+	->enable_db();
