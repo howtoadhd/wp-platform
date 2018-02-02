ded4291e7ac16ac4f69e8f614acf57ffd9368a24
LoreleiAurora <iam@l5i.me>
2018-02-02 17:39:32 +0000

    Add SES WP Mail

--- src/Platform.php
+++ src/Platform.php
@@ -13,6 +13,7 @@ use HowToADHD\WPPlatform\Service\Database;
 use HowToADHD\WPPlatform\Service\ObjectCache;
 use HowToADHD\WPPlatform\Service\PageCache;
 use HowToADHD\WPPlatform\Service\S3Uploads;
+use HowToADHD\WPPlatform\Service\SESMail;
 use HowToADHD\WPPlatform\Service\TaskRunner;
 use HowToADHD\WPPlatform\Service\Service;
 
@@ -120,8 +121,9 @@ final class Platform implements Registerable {
 			'Database'    => Database::class,
 			'ObjectCache' => ObjectCache::class,
 			'PageCache'   => PageCache::class,
-			'TaskRunner'  => TaskRunner::class,
 			'S3Uploads'   => S3Uploads::class,
+			'SESMail'     => SESMail::class,
+			'TaskRunner'  => TaskRunner::class,
 		];
 	}
 
